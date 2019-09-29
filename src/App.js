6cf6346a39c76a52dec9a4935dc7eb8d357eb740
Kevin Edwards <kedwards@kevinedwards.ca>
2019-09-29 15:23:14 -0600

    basic setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+const App = () => (
+    <Fragment>
+        <h1>Hello World HotLoaded!</h1>
+    </Fragment>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+let path = require("path");
+let webpack = require("webpack");
+let HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: ["webpack/hot/dev-server", "./src/index.js"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+        chunkFilename: "[id].chunk.js",
+        sourceMapFilename: "[name].map",
+    },
+    module: {
+        rules: [
+            { test: /\.(jsx?)$/, use: "babel-loader" },
+            { test: /\.s?css$/, use: ["style-loader", "css-loader"] },
+            { test: /\.(png|svg|jpg|gif)$/, use: ["file-loader"] },
+        ],
+    },
+    mode: "development",
+    devtool: "eval-source-map",
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src/index.template.html"),
+            filename: "index.html",
+        }),
+    ],
+    resolve: { extensions: ["*", ".js", ".jsx", ".css", ".scss"] },
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        port: 3000,
+        inline: true,
+        publicPath: "/",
+        hotOnly: true,
+        open: true,
+    },
+};
