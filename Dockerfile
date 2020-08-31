12242e77521ea961de30ca57d4460f2d89b291e4
Kevin Edwards <kedwards@kevinedwards.ca>
2020-08-30 18:41:00 -0600

    prepare for deployment via docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,30 @@
-FROM node
+FROM node:14 as builder
 
-WORKDIR /app
+COPY package*.json ./
 
-ENV PATH /app/node_modules/.bin:$PATH
-
-COPY package*.json .npmrc ./
+COPY .npmrc .npmrc
 
 RUN npm install
 
-EXPOSE 3000
+COPY . .
 
-CMD ["npm", "run", "start"]
+RUN npm run build
+
+# The instructions for second stage
+FROM nginx
+
+ARG GIT_COMMIT=
+ARG BUILD_DATE=
+ARG SOURCE=https://github.com/kedwards/socialize-client
+ARG VERSION=0.0.0
+
+LABEL com.kedwars.socialize-server.revision=$GIT_COMMIT
+LABEL com.kedwars.socialize-server.created=$BUILD_DATE
+LABEL com.kedwars.socialize-server.source=$SOURCE
+LABEL com.kedwars.socialize-server.version=$VERSION
+LABEL com.kedwars.socialize-server.vendor="LivITy Consulting"
+LABEL com.kedwars.socialize-server.authors="Kevin Edwards <kedwards@kevinedwards.ca"
+
+COPY --from=builder ./build /usr/share/nginx/html
+
+COPY docker/nginx/default.conf /etc/nginx/conf.d/default.conf
--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,60 @@ let webpack = require("webpack");
 let HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: ["webpack/hot/dev-server", "./src/index.js"],
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "[name].bundle.js",
-        chunkFilename: "[id].chunk.js",
-        sourceMapFilename: "[name].map",
-    },
-    module: {
-        rules: [
-            { test: /\.(jsx?)$/, use: "babel-loader" },
-            { test: /\.s?css$/, use: ["style-loader", "css-loader"] },
-            { test: /\.(png|svg|jpg|gif)$/, use: ["file-loader"] },
-        ],
-    },
-    mode: "development",
-    devtool: "eval-source-map",
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src/index.template.html"),
-            filename: "index.html",
-        }),
+  entry: ["webpack/hot/dev-server", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].bundle.js",
+    chunkFilename: "[id].chunk.js",
+    sourceMapFilename: "[name].map",
+  },
+  module: {
+    rules: [{
+        test: /\.(jsx?)$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
     ],
-    resolve: { extensions: ["*", ".js", ".jsx", ".css", ".scss"] },
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        port: 3000,
-        inline: true,
-        publicPath: "/",
-        historyApiFallback: true,
-        hotOnly: true,
-        open: true,
-        host : "0.0.0.0",
-        proxy: {
-            "/api": {
-                host: "api",
-                protocol: "http:",
-                port: 5000,
-                target: "http://api:5000",
-            },
-            ignorePath: true,
-            changeOrigin: true,
-            secure: false
-
-        },
+  },
+  mode: "development",
+  devtool: "eval-source-map",
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/index.template.html"),
+      filename: "index.html",
+    }),
+  ],
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".css", ".scss"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    port: 3000,
+    inline: true,
+    publicPath: "/",
+    historyApiFallback: true,
+    hotOnly: true,
+    open: true,
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        host: "0.0.0.0",
+        protocol: "http:",
+        port: 5000,
+        target: "http://server:5000",
+      },
+      ignorePath: true,
+      changeOrigin: true,
+      secure: false
     },
+    disableHostCheck: true
+  },
 };
